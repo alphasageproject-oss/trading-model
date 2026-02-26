b5c53921eea9a7410ae6b39ce4fd8ce10995a6c1
alphasageproject-oss <alphasageproject@gmail.com>
2026-02-27 00:32:47 +0530

    Create base.py
    Create base.py

---
+++ model/base.py
@@ -0,0 +1,7 @@
+class BaseModel:
+    """A base model class that can be inherited by other models."""
+
+    def __init__(self):
+        pass
+
+    # You can define common methods or properties here

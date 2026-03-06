ee782612966b1c17226eb239e0ff7119781f6e69
alphasageproject-oss <alphasageproject@gmail.com>
2026-03-07 01:46:07 +0530

    Create moving_average_weekly.py

---
+++ models/moving_average_weekly.py
@@ -0,0 +1,13 @@
+from .base import Base, Column, Integer, Float, Date, ForeignKey, relationship
+
+class MovingAverageWeekly(Base):
+    __tablename__ = 'moving_averages_weekly'
+    
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, ForeignKey('stocks.id'), nullable=False)
+    date = Column(Date, nullable=False)
+    ma10_value = Column(Float)
+    ma50_value = Column(Float)
+    ma200_value = Column(Float)
+    
+    stock = relationship("Stock", back_populates="ma_weekly")

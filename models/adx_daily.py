f9120177b46fcf3dbfb119c486e85273e370858e
alphasageproject-oss <alphasageproject@gmail.com>
2026-03-07 00:51:10 +0530

    Create adx_daily.py

---
+++ models/adx_daily.py
@@ -0,0 +1,13 @@
+from .base import Base, Column, Integer, Float, Date, ForeignKey, relationship
+
+class ADXDaily(Base):
+    __tablename__ = 'adx_daily'
+    
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, ForeignKey('stocks.id'), nullable=False)
+    date = Column(Date, nullable=False)
+    adx_value = Column(Float)
+    plus_di = Column(Float)
+    minus_di = Column(Float)
+    
+    stock = relationship("Stock", back_populates="adx_daily")

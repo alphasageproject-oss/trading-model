594a86ed535afc095b551944660ee3c5e7e768e5
alphasageproject-oss <alphasageproject@gmail.com>
2026-03-07 02:16:02 +0530

    Create init.py

---
+++ controllers/init.py
@@ -0,0 +1,4 @@
+from .populate_daily_indicators import populate_daily_indicators
+from .populate_weekly_indicators import populate_weekly_indicators
+
+__all__ = ['populate_daily_indicators', 'populate_weekly_indicators']

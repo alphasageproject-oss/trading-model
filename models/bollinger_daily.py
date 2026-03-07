ed3365f0cfb9ca5baa16506a82183e3c625a5e6c
alphasageproject-oss <alphasageproject@gmail.com>
2026-03-07 13:13:09 +0530

    Update bollinger_daily.py

--- models/bollinger_daily.py
+++ models/bollinger_daily.py
@@ -11,3 +11,7 @@ class BollingerDaily(Base):
     lower_band = Column(Float)
     
     stock = relationship("Stock", back_populates="bollinger_daily")
+    
+    __table_args__ = (
+        UniqueConstraint('stock_id', 'date', name='uq_bollinger_daily_stock_date'),
+    )

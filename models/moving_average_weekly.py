7324e6e2d382363179fa0925ab4eaa3feb674efd
alphasageproject-oss <alphasageproject@gmail.com>
2026-03-07 13:16:54 +0530

    Update moving_average_weekly.py

--- models/moving_average_weekly.py
+++ models/moving_average_weekly.py
@@ -11,3 +11,7 @@ class MovingAverageWeekly(Base):
     ma200_value = Column(Float)
     
     stock = relationship("Stock", back_populates="ma_weekly")
+    
+    __table_args__ = (
+        UniqueConstraint('stock_id', 'date', name='uq_macd_weekly_stock_date'),
+    )
